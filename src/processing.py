dc0abb57c2b0b1adc8da34c6ce6d591e401c25a0
Виталий Криворотов <krivorotovv2002@gmail.com>
2025-01-04 22:18:57 +0500

    removing unnecessary branches

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,16 @@
-from typing import List, Dict, Any
 from datetime import datetime
+from typing import Any, Dict, List
 
-def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Возвращает новый список словарей, содержит те словари, у которых ключ state соответствует указанному значению"""
     filtered_data: List[Dict[str, Any]] = []
     for item in data:
-        if 'state' in item and item['state'] == state:
+        if "state" in item and item["state"] == state:
             filtered_data.append(item)
     return filtered_data
 
 
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Возвращать новый список, отсортированный по дате"""
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
