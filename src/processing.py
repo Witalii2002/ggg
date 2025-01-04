835d99d47ce034635b85f4b390497033b5bf0e44
Виталий Криворотов <krivorotovv2002@gmail.com>
2025-01-05 00:23:02 +0500

    homework 10_1 done(final)

--- src/processing.py
+++ src/processing.py
@@ -2,15 +2,16 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
-    """Возвращает новый список словарей, по ключу  state"""
-    filtered_data: List[Dict[str, Any]] = []
-    for item in data:
+def filter_by_state(input_data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует
+    указанному значению"""
+    filtered_data = []
+    for item in input_data:
         if "state" in item and item["state"] == state:
             filtered_data.append(item)
     return filtered_data
 
 
-def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
-    """Возвращать новый список, отсортированный по дате"""
-    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
+def sort_by_date(input_data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """возвращать новый список, отсортированный по дате"""
+    return sorted(input_data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
