8d1caa2040c7e28da2d308150a8918203be76eb8
Виталий Криворотов <krivorotovv2002@gmail.com>
2025-02-11 19:00:26 +0500

    add new test and README.md(final)

--- src/generators.py
+++ src/generators.py
@@ -2,7 +2,7 @@ def filter_by_currency(transactions, currency_code):
     """Возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)"""
     for transaction in transactions:
         try:
-            if transaction['operationAmount']['currency']['code'] == currency_code:
+            if transaction["operationAmount"]["currency"]["code"] == currency_code:
                 yield transaction
         except (KeyError, TypeError):
             continue
@@ -12,7 +12,7 @@ def transaction_descriptions(transactions):
     """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     for transaction in transactions:
         try:
-            yield transaction['description']
+            yield transaction["description"]
         except KeyError:
             yield "Описание отсутствует"
 
@@ -21,5 +21,5 @@ def card_number_generator(start, end):
     """Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     for i in range(start, end + 1):
         card_number = str(i).zfill(16)  # Заполняем нулями до 16 цифр
-        formatted_card_number = " ".join([card_number[j:j+4] for j in range(0, 16, 4)])
+        formatted_card_number = " ".join([card_number[j : j + 4] for j in range(0, 16, 4)])
         yield formatted_card_number
